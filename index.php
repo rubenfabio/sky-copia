4be09d5dd03af8060be270e00281e3c36f9aea60
ruben fabio gutierrez gongora <ruben.fabio2016@gmail.com>
2022-01-05 14:32:32 -0300

    firt commit

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Central de Vendas SKY- Assine SKY Agora</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+</head>
+<body>
+    <?php include ('includes/header.html'); ?>
+
+    
+    <main>
+        <section class="home-slider">
+            <div style="height: 139px; width:100%"></div>
+            <div class="swiper navigationSlider">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <a href="#">
+                            <img class="w-full h-full" src="assets/images/loja-sky-tv-banner-conforto.png" alt="Copel Telecom Internet 200 mega e 300 mega">
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#">
+                            <img class="w-full h-full " src="assets/images/loja-sky-tv-banner-globo-bbb-payperview.png" alt="Copel Telecom Internet 200 mega">
+                        </a>
+                    </div>
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </section>
+
+        <section id="1">
+            <div class="container">
+                <div class="column gap-y-7" style="text-align:center">
+                    <h2 class="text-sky text-3xl font-semibold">ESCOLHA SEU PLANO PÓS PAGO SKY</h2>
+                    <div>
+                        <p class="text-2xl">Assine SKY e leve o melhor da TV para sua casa!</p>
+                        <p class="text-2xl">A maior programação HD com ﬁlmes, séries, documentários e muito mais.</p>
+                    </div>  
+
+                    <h2 class="text-sky text-3xl font-semibold">SKY TV - TECNOLOGIA HD</h2>
+                    <div>
+                        <p class="text-2xl">Assine SKY TV e leve o melhor da TV para sua casa!</p>
+                        <p class="text-2xl">A maior programação de filmes, séries, documentários e muito mais em alta qualidade é só na SKY TV.</p>
+                    </div>
+                    <h2 class="text-sky text-3xl font-semibold">SKY TV - TECNOLOGIA SD</h2>
+                    <div>
+                        <p class="text-2xl">Assine SKY TV e leve o melhor do entretenimento para sua casa! A maior programação de filmes, séries,</p>
+                        <p class="text-2xl">documentários e muito mais em tecnologia Digital (SD) é só na SKY TV.</p>
+                    </div>
+                    <div class="py-12 flex place-content-center px-2 md:px-0">
+                        <a class="flex md:block bg-sky py-4 px-14 text-center uppercase text-white font-semibold text-lg hover:grayscale-[30%]" href="#">
+                            Veja todos os planos
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="2">
+            <div class="container w-full border-dotted border" style="height:1px">
+            </div>
+        </section>
+        
+       <section id="3">
+        <div class="container">
+                <div class="flex-col py-14 px-5 md:px-0 md:flex-row flex">
+                    <div class="w-full md:w-7/12 md:px-10">
+                        <h2 class="text-sky text-3xl font-semibold py-4">
+                        O MELHOR DA PROGRAMAÇÃO DE TV EM ALTA DEFINIÇÃO É SÓ NA <span class="font-bold">SKY</span>
+                        </h2>
+                        <p class="text-lg font-semibold">
+                            A melhor e mais completa programação para toda a sua família assistir em Alta Definição além dos canais HBO e Telecine para assistir as melhores series e Filmes.
+                        </p>
+                        <p class="text-lg font-semibold">
+                            Aproveite também milhares de filmes e series no <span class="font-bold">SKY PLAY</span> para você assistir quando e onde quiser.
+                        </p>
+                        <div class="flex place-content-end" >
+                            <a class="flex place-items-center text-lg text-sky hover:grayscale-[60%]" href="">Saiba Mais <i class="fas fa-arrow-right px-2"></i></a>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-5/12 md:px-5">
+                        <img class="w-full" src="assets/images/sky_tv_sky_play_2.png" alt="" srcset="">
+                    </div>
+                </div>
+            </div>
+       </section>
+       <section id="4 " class="bg-sky-grey">
+           <div class="container">
+               <div class="flex flex-wrap text-white py-10 gap-y-6">
+
+                   <div class="w-full text-center py-16 px-5">
+                       <h1 class="text-3xl">PORQUE A <span class="font-bold">SKY TV</span> É PERFEITA PARA VOCÊ</h1>
+                   </div>
+
+                    <div class="w-full md:w-6/12 flex-col px-5 md:px-10">
+                        <figure class="flex place-content-center"> 
+                            <img src="assets/images/icon_programacao.png" alt="">
+                        </figure>
+                        <h2 class="text-center text-2xl py-4 font-semibold">OS MELHORES CANAIS ESTÃO NA SUA SKY TV</h2>
+                        <p>São mais de 180 canais de filmes, esportes, documentários e variedades que mais agradam você e sua família.</p>
+                    </div>
+
+                    <div class="w-full md:w-6/12 flex-col px-5 md:px-10">
+                        <figure class="flex place-content-center"> 
+                            <img src="assets/images/icon_hd.png" alt="">
+                        </figure>
+                        <h2 class="text-center text-2xl py-4 font-semibold">IMAGEM EM ALTA DEFINIÇÃO</h2>
+                        <p>Som e imagem em qualidade HD para você aproveitar muito mais sua programação SKY TV.</p>
+                    </div>
+                    <div class="w-full border-dotted border" style="height:1px">
+                    </div>
+                    <div class="w-full md:w-6/12 flex-col px-5 md:px-10">
+                        <figure class="flex place-content-center"> 
+                            <img src="assets/images/icon_play.png" alt="">
+                        </figure>
+                        <h2 class="text-center text-2xl py-4 font-semibold">ASSISTA SUA PROGRAMAÇÃO EM QUALQUER HORA E EM QUALQUER LUGAR</h2>
+                        <p>Assista quando e onde quiser na sua TV, tablet, smartphone e notebook com SKY PLAY.</p>
+                    </div>
+
+                    <div class="w-full md:w-6/12 flex-col px-5 md:px-10">
+                        <figure class="flex place-content-center"> 
+                            <img src="assets/images/icon_rec.png" alt="">
+                        </figure>
+                        <h2 class="text-center text-2xl py-4 font-semibold">SKY GRAVADOR</h2>
+                        <p>Não vai conseguir assistir aquele programa tão esperado? Fique tranquilo! com a SKY TV você pode gravar seu programa preferido e assistir a hora que desejar.</p>
+                    </div>
+                    
+               </div>
+           </div>
+       </section>
+
+       <section id="5">
+            <div class="container">
+                <div class="flex-col py-14 px-5 md:px-0 md:flex-row flex">
+                    <div class="w-full md:w-5/12 md:px-5">
+                        <img class="w-full" src="assets/images/sky_tv_sky_play_2.png" alt="" srcset="">
+                    </div>
+                    <div class="w-full md:w-7/12 md:px-10">
+                        <h2 class="text-sky text-3xl font-semibold py-4">
+                            SÓ NA <span class="font-bold">SKY</span> TV VOCÊ TEM A MAIS VARIADA PROGRAMAÇÃO EM ALTA DEFINIÇÃO
+                        </h2>
+                        <p class="text-lg font-semibold">
+                            Muito conteúdo com séries, filmes, infantis, documentários e muito mais. E ainda assista os maiores lançamentos recém saídos do cinema no <span class="font-bold">SKY PLAY</span> onde e quando quiser.
+                        </p>
+                        <div class="flex place-content-end" >
+                            <a class="flex place-items-center text-lg text-sky hover:grayscale-[60%]" href="">Saiba Mais <i class="fas fa-arrow-right px-2"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </section>
+
+    </main>
+    
+    <?php include ('includes/footer.html'); ?>
+    <script>
+    function darktheme() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+        }
+        var swiper = new Swiper(".navigationSlider", {
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: true,
+            },
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+            },
+            loop: true,
+            pagination: {
+            el: ".swiper-pagination",
+            },
+        });
+    </script>
+</body>
+</html>
